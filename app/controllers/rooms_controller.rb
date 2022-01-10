8d560d7c983426454002d3881099f6811e8acf1f
fyky <fuyu12dmb25ms@gmail.com>
2022-01-10 16:47:48 +0900

    [Fix]RoomUser値を渡す

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,21 @@
 class RoomsController < ApplicationController
   def create
     @room = Room.create
+    # RoomUserを2つ作る
+    # 自分のRoomUserを作る
+    @join_current_user = RoomUser.create(user_id: current_user.id, room_id: @room.id)
+    # 相手のRoomUserを作る
+    @join_user = RoomUser.create(join_room_params)
     redirect_to room_path(@room)
   end
 
   def show
     @room = Room.find(params[:id])
+  end
+
+  private
+
+  def join_room_params
+    params.require(:room_user).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 end
